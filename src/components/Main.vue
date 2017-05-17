92436e92e4c718bd8b3d431040d7fe910b948335
Anastasia Kozaeva <sacret190990@gmail.com>
2017-05-18 02:45:36 +0300

    Initialize Vue js project with material design

---
+++ src/components/Main.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-container>
+    <v-layout row wrap>
+      <v-flex xs12>
+        <h1>{{ msg }}</h1>
+        <h2>Essential Links</h2>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data() {
+    return {
+      msg: 'Planning',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
