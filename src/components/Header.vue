1237c26518171eda27040e0acb330972145254c8
Anastasia Kozaeva <sacret190990@gmail.com>
2017-06-15 00:03:47 +0300

    Add icons and change font

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,8 +2,12 @@
   <v-toolbar class="orange" light>
     <v-toolbar-title></v-toolbar-title>
     <v-toolbar-items class="hidden-md-and-down">
-      <v-toolbar-item ripple>Link</v-toolbar-item>
-      <v-toolbar-item ripple>Link</v-toolbar-item>
+      <v-btn icon light>
+        <v-icon>share</v-icon>
+      </v-btn>
+      <v-btn icon light>
+        <v-icon>star</v-icon>
+      </v-btn>
       <v-toolbar-item ripple v-if="userName">{{ userName }}</v-toolbar-item>
     </v-toolbar-items>
   </v-toolbar>
