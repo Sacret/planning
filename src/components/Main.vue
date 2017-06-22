5a480cf551a7ef5d445ccb8ab907f8d845c7af45
Anastasia Kozaeva <sacret190990@gmail.com>
2017-06-22 23:13:28 +0300

    Remove useless code

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -26,7 +26,6 @@
 import Firebase from 'firebase';
 import LoginForm from '@/components/LoginForm';
 import PlanningForm from '@/components/PlanningForm';
-// import db from '../firebase';
 
 export default {
   name: 'main',
@@ -45,8 +44,6 @@ export default {
     Firebase.auth().onAuthStateChanged((user) => {
       if (user) {
         this.user = user;
-        // this.$bindAsArray('plannings', db.ref(`plannings/${user.uid}`));
-        debugger;
         this.$store.commit('saveUserId', { uid: user.uid });
       } else {
         Firebase.auth().signInAnonymously().catch(console.error);
