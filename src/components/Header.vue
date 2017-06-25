c085d3aa538ff5c5253c16c3c1fd390d056a63df
Anastasia Kozaeva <sacret190990@gmail.com>
2017-06-25 12:30:08 +0300

    Add share links

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,12 +2,35 @@
   <v-toolbar class="orange" light>
     <v-toolbar-title class="text-xs-left">{{ planningTitle }}</v-toolbar-title>
     <v-toolbar-items>
-      <v-btn icon light>
-        <v-icon>share</v-icon>
-      </v-btn>
-      <v-btn icon light>
-        <v-icon>star</v-icon>
-      </v-btn>
+      <v-menu left bottom origin="bottom right" transition="v-scale-transition">
+        <v-btn light icon slot="activator">
+          <v-icon>share</v-icon>
+        </v-btn>
+        <v-list>
+          <template v-for="socialLink in socialLinks">
+            <v-list-item>
+              <v-list-tile>
+                <v-list-tile-title>
+                  <social-sharing :url="sharingURL" inline-template>
+                    <network :network="socialLink">
+                      {{ socialLink | capitalize }}
+                    </network>
+                  </social-sharing>
+                </v-list-tile-title>
+              </v-list-tile>
+            </v-list-item>
+          </template>
+        </v-list>
+      </v-menu>
+      <v-toolbar-item>
+        <a
+          href="https://github.com/Sacret/planning"
+          target="_blank"
+          light
+        >
+          <v-icon>star</v-icon>
+        </a>
+      </v-toolbar-item>
       <v-toolbar-item ripple v-if="userName">{{ userName }}</v-toolbar-item>
       <v-menu left bottom origin="bottom right" transition="v-scale-transition" v-if="userName">
         <v-btn light icon slot="activator">
@@ -28,6 +51,10 @@
 <script>
 export default {
   name: 'header',
+  data: () => ({
+    sharingURL: window.location.origin,
+    socialLinks: ['facebook', 'twitter', 'vk'],
+  }),
   computed: {
     userName() {
       return this.$store.state.userName;
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify';
 import VueFire from 'vuefire';
+import VueSocialSharing from 'vue-social-sharing';
 
 import App from './App';
 import router from './router';
@@ -11,6 +12,13 @@ import store from './store';
 Vue.config.productionTip = false;
 Vue.use(Vuetify);
 Vue.use(VueFire);
+Vue.use(VueSocialSharing);
+
+Vue.filter('capitalize', (value) => {
+  if (!value) return '';
+  const newValue = value.toString();
+  return newValue.charAt(0).toUpperCase() + newValue.slice(1);
+});
 
 /* eslint-disable no-new */
 new Vue({
